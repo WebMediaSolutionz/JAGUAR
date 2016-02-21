7fa6642a8e3283917dffc564a00f59fb5f056e50
Maxime Pierre <x182171@corp.ads>
2016-02-21 18:31:27 -0500

    [JPP-571]: Validate fields where addresses are entered

--- partials/header.php
+++ partials/header.php
@@ -3,24 +3,27 @@
 	<head>
 		<title>TELUS</title>
 
-		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/reset.css">
-		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/frogger.css">
-		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/telus-fonts.css">
-		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/telus-bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/bootstrap-datepicker.standalone.min.css">
-		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/dropit.css">
-		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/slick.css">
-		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/slick-theme.css">
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/reset.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/frogger.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/telus-fonts.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/telus-bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/bootstrap-datepicker.standalone.min.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/dropit.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/slick.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/slick-theme.css" />
 
-		<link rel="stylesheet" type="text/css" href="stylesheet/css/styles.css">
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/styles.css" />
 
-		<link rel="stylesheet" type="text/css" href="//static.telus.com/common/css/sandboxes/fonts.css?v=1.2">
+		<link rel="stylesheet" type="text/css" href="//static.telus.com/common/css/sandboxes/fonts.css?v=1.2" />
 
-		<link href="images/TELUS-favicon-16.png" sizes="16x16" type="image/png" rel="icon">
+		<link href="images/TELUS-favicon-16.png" sizes="16x16" type="image/png" rel="icon" />
 
 		<!--[if lt IE 9]><script type="text/javascript" src="javascript/lib/html5shim.js"></script><![end if]-->
 	</head>
 	<body>
+		<link rel="stylesheet" type="text/css" href="http://ws1.postescanada-canadapost.ca/css/addresscomplete-2.30.min.css?key=kt58-ah69-ht47-ye97" />
+		<script type="text/javascript" src="http://ws1.postescanada-canadapost.ca/js/addresscomplete-2.30.min.js?key=kt58-ah69-ht47-ye97"></script>
+		
 		<div class="header_bg white_bg">
 			<div class="outer-container header frg-drop-shadow">
 				<div class="inner-container">
