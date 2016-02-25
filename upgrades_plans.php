d722ad5b6f4b441ea6016cdb1751c42d029e15f9
Maxime Pierre <x182171@corp.ads>
2016-02-25 13:58:15 -0500

    renaming some of those pages

---
+++ upgrades_plans.php
@@ -0,0 +1,202 @@
+<?php include_once( 'config.php' ); ?>
+<?php include_once( 'partials/header.php' ); ?>
+
+<?php include( 'partials/check_progress_bar.php' ); ?>
+
+<input type="hidden" name="nbr_device" value="50" />
+
+<div class="outer-container secondary_bg purple_bg">
+	<div class="inner-container clearfix">
+		<h3 class="title">Select a plan for:</h3>
+		<p class="h3"><strong>Group-1 Galaxy Prime (50 devices)</strong> <input class='frg-input-field hide js-auto_save' placeholder="press 'enter' to save" value='Order-1 Galaxy Prime (50 devices)'/> <a class="small edit_name" href="#">edit name</a></p>
+	</div>
+</div>
+
+<div class="outer-container content white_bg top_bottom_small_padding">
+	<div class="inner-container clearfix">
+		<span class="gray_text small">Minimum Monthly Service Commitment: $20/subscriber</span>
+	</div>
+</div>
+
+<div class="outer-container content white_bg">
+	<div class="inner-container clearfix">
+		<div class="plan clearfix frg-drop-shadow">
+			<div class="col-xs-5 top_bottom_padding">
+				<p class="h2 big">Unlimited nation wide minutes</p>
+				<p>The right choice for occasional local calls. Plus you get unlimited weelemds and evenings after 6pm.</p>
+			</div>
+			<div class="col-xs-4 top_bottom_padding">
+				<p class="big">Features included:</p>
+				<ul class="no_styles">
+					<li><span class="frg-icon icon-checkmark"></span> no commitment, contracts or credit checkes</li>
+					<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
+					<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+				</ul>
+
+				<div>
+					<ul class="extra_features no_styles">
+						<li><span class="frg-icon icon-checkmark"></span> no commitment, contracts or credit checkes</li>
+						<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
+						<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+						<li><span class="frg-icon icon-checkmark"></span> no commitment, contracts or credit checkes</li>
+						<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
+						<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+					</ul>
+
+					<a class="js-view_more closed gap_left" href="#"><span class="text">View all</span> <span class="frg-icon icon-plus-circled purple_text"></span></a>
+				</div>
+			</div>
+			<div class="col-xs-3 right_align">
+				<div class="popular right">Popular</div>
+				<div class="clearfix"></div>
+				<div><h6 class="price"><span>$60</span> per month</h6></div>
+				<a class="frg-button select" href="#">Select</a>
+			</div>
+			<input type="hidden" name="price_per_month" value="60" />
+		</div>
+
+		<div class="plan clearfix frg-drop-shadow">
+			<div class="col-xs-5 top_bottom_padding">
+				<p class="h2 big">Unlimited nation wide minutes</p>
+				<p>The right choice for occasional local calls. Plus you get unlimited weelemds and evenings after 6pm.</p>
+			</div>
+			<div class="col-xs-4 top_bottom_padding">
+				<p class="big">Features included:</p>
+				<ul class="no_styles">
+					<li><span class="frg-icon icon-checkmark"></span> no commitment, contracts or credit checkes</li>
+					<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
+					<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+				</ul>
+
+				<div>
+					<ul class="extra_features no_styles">
+						<li><span class="frg-icon icon-checkmark"></span> no commitment, contracts or credit checkes</li>
+						<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
+						<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+						<li><span class="frg-icon icon-checkmark"></span> no commitment, contracts or credit checkes</li>
+						<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
+						<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+					</ul>
+
+					<a class="js-view_more closed gap_left" href="#"><span class="text">View all</span> <span class="frg-icon icon-plus-circled purple_text"></span></a>
+				</div>
+			</div>
+			<div class="col-xs-3 right_align">
+				<div class="popular right invisible">Popular</div>
+				<div class="clearfix"></div>
+				<div><h6 class="price"><span>$55</span> per month</h6></div>
+				<a class="frg-button select" href="#">Select</a>
+			</div>
+			<input type="hidden" name="price_per_month" value="55" />
+		</div>
+
+		<div class="plan clearfix frg-drop-shadow">
+			<div class="col-xs-5 top_bottom_padding">
+				<p class="h2 big">Unlimited nation wide minutes</p>
+				<p>The right choice for occasional local calls. Plus you get unlimited weelemds and evenings after 6pm.</p>
+			</div>
+			<div class="col-xs-4 top_bottom_padding">
+				<p class="big">Features included:</p>
+				<ul class="no_styles">
+					<li><span class="frg-icon icon-checkmark"></span> no commitment, contracts or credit checkes</li>
+					<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
+					<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+				</ul>
+
+				<div>
+					<ul class="extra_features no_styles">
+						<li><span class="frg-icon icon-checkmark"></span> no commitment, contracts or credit checkes</li>
+						<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
+						<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+						<li><span class="frg-icon icon-checkmark"></span> no commitment, contracts or credit checkes</li>
+						<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
+						<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+					</ul>
+
+					<a class="js-view_more closed gap_left" href="#"><span class="text">View all</span> <span class="frg-icon icon-plus-circled purple_text"></span></a>
+				</div>
+			</div>
+			<div class="col-xs-3 right_align">
+				<div class="popular right invisible">Popular</div>
+				<div class="clearfix"></div>
+				<div><h6 class="price"><span>$50</span> per month</h6></div>
+				<a class="frg-button select" href="#">Select</a>
+			</div>
+			<input type="hidden" name="price_per_month" value="50" />
+		</div>
+
+		<div class="plan clearfix frg-drop-shadow">
+			<div class="col-xs-5 top_bottom_padding">
+				<p class="h2 big">Unlimited nation wide minutes</p>
+				<p>The right choice for occasional local calls. Plus you get unlimited weelemds and evenings after 6pm.</p>
+			</div>
+			<div class="col-xs-4 top_bottom_padding">
+				<p class="big">Features included:</p>
+				<ul class="no_styles">
+					<li><span class="frg-icon icon-checkmark"></span> no commitment, contracts or credit checkes</li>
+					<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
+					<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+				</ul>
+
+				<div>
+					<ul class="extra_features no_styles">
+						<li><span class="frg-icon icon-checkmark"></span> no commitment, contracts or credit checkes</li>
+						<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
+						<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+						<li><span class="frg-icon icon-checkmark"></span> no commitment, contracts or credit checkes</li>
+						<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
+						<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+					</ul>
+
+					<a class="js-view_more closed gap_left" href="#"><span class="text">View all</span> <span class="frg-icon icon-plus-circled purple_text"></span></a>
+				</div>
+			</div>
+			<div class="col-xs-3 right_align">
+				<div class="popular right invisible">Popular</div>
+				<div class="clearfix"></div>
+				<div><h6 class="price"><span>$45</span> per month</h6></div>
+				<a class="frg-button select" href="#">Select</a>
+			</div>
+			<input type="hidden" name="price_per_month" value="45" />
+		</div>
+
+		<div class="plan clearfix frg-drop-shadow">
+			<div class="col-xs-5 top_bottom_padding">
+				<p class="h2 big">Unlimited nation wide minutes</p>
+				<p>The right choice for occasional local calls. Plus you get unlimited weelemds and evenings after 6pm.</p>
+			</div>
+			<div class="col-xs-4 top_bottom_padding">
+				<p class="big">Features included:</p>
+				<ul class="no_styles">
+					<li><span class="frg-icon icon-checkmark"></span> no commitment, contracts or credit checkes</li>
+					<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
+					<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+				</ul>
+
+				<div>
+					<ul class="extra_features no_styles">
+						<li><span class="frg-icon icon-checkmark"></span> no commitment, contracts or credit checkes</li>
+						<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
+						<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+						<li><span class="frg-icon icon-checkmark"></span> no commitment, contracts or credit checkes</li>
+						<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
+						<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+					</ul>
+
+					<a class="js-view_more closed gap_left" href="#"><span class="text">View all</span> <span class="frg-icon icon-plus-circled purple_text"></span></a>
+				</div>
+			</div>
+			<div class="col-xs-3 right_align">
+				<div class="popular right invisible">Popular</div>
+				<div class="clearfix"></div>
+				<div><h6 class="price"><span>$40</span> per month</h6></div>
+				<a class="frg-button select" href="#">Select</a>
+			</div>
+			<input type="hidden" name="price_per_month" value="40" />
+		</div>
+	</div>
+</div>
+
+<?php include( 'partials/checkout_sticky.php' ); ?>
+
+<?php include( 'partials/footer.php' ); ?>
