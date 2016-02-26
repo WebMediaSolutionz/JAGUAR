ada01ade0fb64657e1bd832e1fcd9ea3a5752277
Maxime Pierre <x182171@corp.ads>
2016-02-26 17:59:41 -0500

    [JPP-622]: Update the boxes so that text wraps and the height stays the same across the board

--- addons.php
+++ addons.php
@@ -52,7 +52,7 @@
 			</div>
 
 			<div>
-				<p>Link service turns your mobile phone into a two-way radio so you can connect to individuals or teams with the push of a button from virtually anywhere in North America.
+				<p class="js-ellipsis" data-maxlen="175">Link service turns your mobile phone into a two-way radio so you can connect to individuals or teams with the push of a button from virtually anywhere in North America.
 				</p>
 			</div>
 
@@ -71,7 +71,7 @@
 			</div>
 
 			<div>
-				<p>Link service turns your mobile phone into a two-way radio so you can connect to individuals or teams with the push of a button from virtually anywhere in North America.
+				<p class="js-ellipsis" data-maxlen="175">Link service turns your mobile phone into a two-way radio so you can connect to individuals or teams with the push of a button from virtually anywhere in North America.
 				</p>
 			</div>
 
@@ -90,7 +90,7 @@
 			</div>
 
 			<div>
-				<p>Link service turns your mobile phone into a two-way radio so you can connect to individuals or teams with the push of a button from virtually anywhere in North America.
+				<p class="js-ellipsis" data-maxlen="175">Link service turns your mobile phone into a two-way radio so you can connect to individuals or teams with the push of a button from virtually anywhere in North America.
 				</p>
 			</div>
 
@@ -109,7 +109,159 @@
 			</div>
 
 			<div>
-				<p>Link service turns your mobile phone into a two-way radio so you can connect to individuals or teams with the push of a button from virtually anywhere in North America.
+				<p class="js-ellipsis" data-maxlen="175">Link service turns your mobile phone into a two-way radio so you can connect to individuals or teams with the push of a button from virtually anywhere in North America.
+				</p>
+			</div>
+
+			<br />
+
+			<div class="center_text">
+				<a class="frg-button select" href="#">Add service</a>
+			</div>
+		</div>
+
+		<div data-filter="roaming" class="service object col-xs-3 clearfix frg-drop-shadow">
+			<h3>TELUS link</h3>
+			
+			<div>
+				<h6 class="price"><span>$60</span> per month</h6>
+			</div>
+
+			<div>
+				<p class="js-ellipsis" data-maxlen="175">Link service turns your mobile phone into a two-way radio so you can connect to individuals or teams with the push of a button from virtually anywhere in North America.
+				</p>
+			</div>
+
+			<br />
+
+			<div class="center_text">
+				<a class="frg-button select" href="#">Add service</a>
+			</div>
+		</div>
+
+		<div data-filter="data" class="service object col-xs-3 clearfix frg-drop-shadow">
+			<h3>TELUS link</h3>
+			
+			<div>
+				<h6 class="price"><span>$60</span> per month</h6>
+			</div>
+
+			<div>
+				<p class="js-ellipsis" data-maxlen="175">Link service turns your mobile phone into a two-way radio so you can connect to individuals or teams with the push of a button from virtually anywhere in North America.
+				</p>
+			</div>
+
+			<br />
+
+			<div class="center_text">
+				<a class="frg-button select" href="#">Add service</a>
+			</div>
+		</div>
+
+		<div data-filter="services" class="service object col-xs-3 clearfix frg-drop-shadow">
+			<h3>TELUS link</h3>
+			
+			<div>
+				<h6 class="price"><span>$60</span> per month</h6>
+			</div>
+
+			<div>
+				<p class="js-ellipsis" data-maxlen="175">Link service turns your mobile phone into a two-way radio so you can connect to individuals or teams with the push of a button from virtually anywhere in North America.
+				</p>
+			</div>
+
+			<br />
+
+			<div class="center_text">
+				<a class="frg-button select" href="#">Add service</a>
+			</div>
+		</div>
+
+		<div data-filter="services" class="service object col-xs-3 clearfix frg-drop-shadow">
+			<h3>TELUS link</h3>
+			
+			<div>
+				<h6 class="price"><span>$60</span> per month</h6>
+			</div>
+
+			<div>
+				<p class="js-ellipsis" data-maxlen="175">Link service turns your mobile phone into a two-way radio so you can connect to individuals or teams with the push of a button from virtually anywhere in North America.
+				</p>
+			</div>
+
+			<br />
+
+			<div class="center_text">
+				<a class="frg-button select" href="#">Add service</a>
+			</div>
+		</div>
+
+		<div data-filter="roaming" class="service object col-xs-3 clearfix frg-drop-shadow">
+			<h3>TELUS link</h3>
+			
+			<div>
+				<h6 class="price"><span>$60</span> per month</h6>
+			</div>
+
+			<div>
+				<p class="js-ellipsis" data-maxlen="175">Link service turns your mobile phone into a two-way radio so you can connect to individuals or teams with the push of a button from virtually anywhere in North America.
+				</p>
+			</div>
+
+			<br />
+
+			<div class="center_text">
+				<a class="frg-button select" href="#">Add service</a>
+			</div>
+		</div>
+
+		<div data-filter="data" class="service object col-xs-3 clearfix frg-drop-shadow">
+			<h3>TELUS link</h3>
+			
+			<div>
+				<h6 class="price"><span>$60</span> per month</h6>
+			</div>
+
+			<div>
+				<p class="js-ellipsis" data-maxlen="175">Link service turns your mobile phone into a two-way radio so you can connect to individuals or teams with the push of a button from virtually anywhere in North America.
+				</p>
+			</div>
+
+			<br />
+
+			<div class="center_text">
+				<a class="frg-button select" href="#">Add service</a>
+			</div>
+		</div>
+
+		<div data-filter="services" class="service object col-xs-3 clearfix frg-drop-shadow">
+			<h3>TELUS link</h3>
+			
+			<div>
+				<h6 class="price"><span>$60</span> per month</h6>
+			</div>
+
+			<div>
+				<p class="js-ellipsis" data-maxlen="175">Link service turns your mobile phone into a two-way radio so you can connect to individuals or teams with the push of a button from virtually anywhere in North America.
+				</p>
+			</div>
+
+			<br />
+
+			<div class="center_text">
+				<a class="frg-button select" href="#">Add service</a>
+			</div>
+		</div>
+
+		<div data-filter="services" class="service object col-xs-3 clearfix frg-drop-shadow">
+			<h3>TELUS link</h3>
+			
+			<div>
+				<h6 class="price"><span>$60</span> per month</h6>
+			</div>
+
+			<div>
+				<p class="js-ellipsis" data-maxlen="175">Link service turns your mobile phone into a two-way radio so you can connect to individuals or teams with the push of a button from virtually anywhere in North America.
 				</p>
 			</div>
 
