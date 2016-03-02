f25a04afbc1a6a58692dc6dd303999982b5ad5e9
Maxime Pierre <maxime.pierre@telus.com>
2016-03-02 13:11:57 -0500

    onboarding registration page

---
+++ partials/header2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html class="<?php echo $page; ?> header2" lang="en" data-currentpage="<?php echo $page; ?>">
+	<head>
+		<title>TELUS</title>
+
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/reset.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/frogger.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/telus-fonts.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/telus-bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/bootstrap-datepicker.standalone.min.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/dropit.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/slick.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/lib/slick-theme.css" />
+
+		<link rel="stylesheet" type="text/css" href="stylesheet/css/styles2.css" />
+
+		<link rel="stylesheet" type="text/css" href="//static.telus.com/common/css/sandboxes/fonts.css?v=1.2" />
+
+		<link href="images/TELUS-favicon-16.png" sizes="16x16" type="image/png" rel="icon" />
+
+		<!--[if lt IE 9]><script type="text/javascript" src="javascript/lib/html5shim.js"></script><![end if]-->
+	</head>
+	<body>
+		<link rel="stylesheet" type="text/css" href="http://ws1.postescanada-canadapost.ca/css/addresscomplete-2.30.min.css?key=zh57-pk62-rn95-ny31" />
+		<script type="text/javascript" src="http://ws1.postescanada-canadapost.ca/js/addresscomplete-2.30.min.js?key=zh57-pk62-rn95-ny31"></script>
+
+		<header class="outer-container">
+			<img src="images/TELUS-logo.svg" />
+		</header>
+
+		<div class="outer-container light_gray_bg devil_gray_text">
+			<span>Already have an account? <a href="#">Login</a></span>
+		</div>
---
+++ registration.php
@@ -0,0 +1,62 @@
+<?php include_once( 'config.php' ); ?>
+<?php include_once( 'partials/header2.php' ); ?>
+
+<div class="outer-container main_content">
+	<div class="inner-container">
+		<h2 class="center devil_gray_text line light_gray2 vertical_padding30">First, let's create your profile</h2>
+
+		<div class="row top_margin50">
+			<div class="col-xs-6 center">
+				<img src="images/flower2.jpg" />
+			</div>
+			<div class="col-xs-4 registration_form">
+				<form>
+					<div class="bottom_margin20">
+						<label class="block devil_gray_text">First name</label>
+						<input class='frg-input-field full_width' name='' />
+					</div>
+
+					<div class="bottom_margin20">
+						<label class="block devil_gray_text">Last name</label>
+						<input class='frg-input-field full_width' name='' />
+					</div>
+
+					<div class="bottom_margin20">
+						<label class="block devil_gray_text">Enter email</label>
+						<input class='frg-input-field full_width' name='' />
+					</div>
+
+					<div class="bottom_margin20">
+						<label class="block devil_gray_text">Re-enter email</label>
+						<input class='frg-input-field full_width' name='' />
+					</div>
+
+					<div class="bottom_margin20">
+						<label class="block devil_gray_text">Create your password</label>
+						<input class='frg-input-field full_width' name='' />
+					</div>
+
+					<h5 class="devil_gray_text bottom_margin20 top_margin20">Just in case you forget your profile details...</h5>
+
+					<div class="bottom_margin20">
+						<label class="block devil_gray_text">Choose a security question</label>
+						<input class='frg-input-field full_width' name='' />
+					</div>
+
+					<div class="bottom_margin20">
+						<label class="block devil_gray_text">Answer</label>
+						<input class='frg-input-field full_width' name='' />
+					</div>
+
+					<div class="top_margin20 bottom_margin20">
+						<span class="devil_gray_text">By clicking Continue, you agree to receive My Account updates and to the TELUS <a href="#">Privacy Commitment</a>.</span>
+					</div>
+
+					<a href='#' class='frg-button button-wide color-green bottom_margin50'>Continue</a>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include( 'partials/footer2.php' ); ?>
