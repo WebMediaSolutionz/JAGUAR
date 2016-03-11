dca559726720997b007b86deb727434f0e0a53e2
Maxime Pierre <maxime.pierre@telus.com>
2016-03-11 12:47:13 -0500

    [EOFR-3415]: tweaks on accessories page

--- upgrades_accessories.php
+++ upgrades_accessories.php
@@ -29,12 +29,6 @@
 					</ul>
 				</nav>
 			<?php } ?>
-			<div class='right frg-select-container color-light'>
-				<select class="js-sort-by" autocomplete="off">
-					<option value="device">Sort by: Device</option>
-					<option value="brand">Sort by: Brand</option>
-				</select>
-			</div>
 		</div>
 	</div>
 </div>
