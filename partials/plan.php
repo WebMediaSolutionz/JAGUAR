2fb80320e1b8b6105abef51fd393a015cd9a7259
Maxime Pierre <maxime.pierre@telus.com>
2016-03-21 10:01:04 -0400

    [EOFR-3533]: When view all link is clicked the amount is moved to the bottom

--- partials/plan.php
+++ partials/plan.php
@@ -12,32 +12,34 @@
 
 <div data-filter="<?php echo $service_category; ?>" class="plan object clearfix frg-drop-shadow padding_bottom">
 	<div class="vertical_gap big_gap clearfix">
-		<div class="left">
-			<h5>Local Talk 50</h5>
-			<ul class="no_styles">
-				<li><span class="frg-icon icon-checkmark"></span> 300 sharable local minutes</li>
-				<li><span class="frg-icon icon-checkmark"></span> Call display, voice mail, call waiting,</li>
-				<li><span class="frg-icon icon-checkmark"></span> Access to shared data</li>
-				<li><span class="frg-icon icon-checkmark"></span> Voice mail to text</li>
-				<li><span class="frg-icon icon-checkmark"></span> Roam ready USA</li>
-			</ul>
-
-			<div>
-				<ul class="extra_features no_styles">
-					<li><span class="frg-icon icon-checkmark"></span> No commitment, contracts or credit checks</li>
-					<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
-					<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
-					<li><span class="frg-icon icon-checkmark"></span> No commitment, contracts or credit checks</li>
-					<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
-					<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+		<div class="row">
+			<div class="col-xs-8">
+				<h5>Local Talk 50</h5>
+				<ul class="no_styles">
+					<li><span class="frg-icon icon-checkmark"></span> 300 sharable local minutes</li>
+					<li><span class="frg-icon icon-checkmark"></span> Call display, voice mail, call waiting,</li>
+					<li><span class="frg-icon icon-checkmark"></span> Access to shared data</li>
+					<li><span class="frg-icon icon-checkmark"></span> Voice mail to text</li>
+					<li><span class="frg-icon icon-checkmark"></span> Roam ready USA</li>
 				</ul>
 
-				<a class="js-view_more closed gap_left" href="#"><span class="text">View all</span> <span class="frg-icon icon-plus-circled purple_text"></span></a>
+				<div>
+					<ul class="extra_features no_styles">
+						<li><span class="frg-icon icon-checkmark"></span> No commitment, contracts or credit checks</li>
+						<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
+						<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+						<li><span class="frg-icon icon-checkmark"></span> No commitment, contracts or credit checks</li>
+						<li><span class="frg-icon icon-checkmark"></span> Low calling rates of $0.15 per min</li>
+						<li><span class="frg-icon icon-checkmark"></span> Voice Mail 3, call display, call waiting and conference calling for free</li>
+					</ul>
+
+					<a class="js-view_more closed gap_left" href="#"><span class="text">View all</span> <span class="frg-icon icon-plus-circled purple_text"></span></a>
+				</div>
 			</div>
-		</div>
-		<div class="right_align clearfix vertical_gap_top small_gap right">
-			<div class="left_align">
-				<h6 class="price"><span>$50</span> per month</h6>
+			<div class="col-xs-4 right_align clearfix vertical_gap_top small_gap">
+				<div class="left_align">
+					<h6 class="price"><span>$50</span> per month</h6>
+				</div>
 			</div>
 		</div>
 	</div>
