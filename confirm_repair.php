70f9fa0946cfe5222fe4a6d73c870ca16fc752ed
Maxime Pierre <maxime.pierre@telus.com>
2016-04-12 16:00:30 -0400

    [EOFR-3798]: adding confirm repair page

---
+++ confirm_repair.php
@@ -0,0 +1,89 @@
+<?php include_once( 'config.php' ); ?>
+<?php include_once( 'partials/header.php' ); ?>
+
+<?php include( 'partials/check_progress_bar.php' ); ?>
+
+<div class="outer-container secondary_bg white_bg">
+	<div class="inner-container clearfix">
+		<div class="left">
+			<h4 class="no_padding purple_text">Confirm repair</h4>
+		</div>
+	</div>
+</div>
+
+<div class="outer-container content white_bg order_details small">
+	<div class="inner-container clearfix">
+		<table class="gray_text wide datagrid">
+			<thead class="gray_text">
+				<tr class="wide2">
+					<?php if ( $page !== 'confirm_repair' ) { ?>
+						<th>Select</th>
+					<?php } ?>
+					<th>Device Type</th>
+					<th>
+						<div class="left">IMEI</div> <div class="frg-icon icon-info back-order purple top_margin_minus7 left gap_left_small"></div>
+					</th>
+					<th>Under Waranty</th>
+					<th>Physical Damage</th>
+					<th>Issue/Symptom</th>
+					<th>Loaner Required</th>
+					<th class="right_align">
+						<div class="left">Estimated Repair Fee</div> <div class="frg-icon icon-info back-order purple top_margin_minus7 left gap_left_small"></div>
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+					for ( $i = 0; $i < 3; $i++ ) { 
+						include( 'partials/repair_item_row_1.php' );
+						include( 'partials/repair_item_row_2.php' );
+					}
+				?>
+			</tbody>
+		</table>
+	</div>
+</div>
+
+<div class="outer-container content white_bg order_details small">
+	<div class="inner-container clearfix">
+		<div class="col-xs-6 col-xs-offset-6 top_horizontal_separator light_gray no_padding clearfix">
+			<div class="right">
+				<table class="right_align subtotal">
+					<tr>
+						<td class="longer">Sub total</td>
+						<td class="right_align">$500.00</td>
+					</tr>
+					<tr>
+						<td>Shipping</td>
+						<td class="right_align">Free</td>
+					</tr>
+					<tr>
+						<td>Taxes HST</td>
+						<td class="right_align">$65.00</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="outer-container content white_bg order_details small">
+	<div class="inner-container clearfix">
+		<div class="col-xs-6 col-xs-offset-6 top_horizontal_separator bottom_horizontal_separator light_gray no_padding clearfix">
+			<div class="right">
+				<table class="right_align total">
+					<tr>
+						<td class="longer">Estimated total repair cost</td>
+						<td class="right_align">$565.00</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
+
+<br />
+
+<?php include( 'partials/checkout_sticky.php' ); ?>
+
+<?php include( 'partials/footer.php' ); ?>
--- partials/repair_item_row_1.php
+++ partials/repair_item_row_1.php
@@ -1,14 +1,16 @@
-<tr class="wide">
-	<td>
-		<div class="frg-checkbox regular small">
-			<div class="inner">
-				<input id="chkbox-option-<?php echo $i; ?>" type="checkbox" name="option-<?php echo $i; ?>" value="Option <?php echo $i; ?>">
-				<div class="icon">
-					<span class="frg-icon" aria-hidden="true"></span>
+<tr class="<?php echo ( $page !== 'confirm_repair' ) ? 'wide' : ''; ?>">
+	<?php if ( $page !== 'confirm_repair' ) { ?>
+		<td>
+			<div class="frg-checkbox regular small">
+				<div class="inner">
+					<input id="chkbox-option-<?php echo $i; ?>" type="checkbox" name="option-<?php echo $i; ?>" value="Option <?php echo $i; ?>">
+					<div class="icon">
+						<span class="frg-icon" aria-hidden="true"></span>
+					</div>
 				</div>
 			</div>
-		</div>
-	</td>
+		</td>
+	<?php } ?>
 	<td>
 		<span>Apple iPhone 6S</span>
 	</td>
@@ -19,15 +21,20 @@
 		<span>No<span/>
 	</td>
 	<td>
-		<div class='frg-select-container color-light full_width'>
-			<select class="js-required" autocomplete="off">
-				<option selected>Select</option>
-				<option>Yes</option>
-				<option>No</option>
-			</select>
-		</div>
+		<?php if ( $page !== 'confirm_repair' ) { ?>
+			<div class='frg-select-container color-light full_width'>
+				<select class="js-required" autocomplete="off">
+					<option selected>Select</option>
+					<option>Yes</option>
+					<option>No</option>
+				</select>
+			</div>
+		<?php } else { ?>
+			<span class="gray_shade1_text">Yes</span>
+		<?php } ?>
 	</td>
 	<td>
+		<?php if ( $page !== 'confirm_repair' ) { ?>
 		<div class='frg-select-container color-light full_width'>
 			<select class="js-required" autocomplete="off">
 				<option selected>Select reason</option>
@@ -36,15 +43,22 @@
 				<option>Lorem ipsum 3</option>
 			</select>
 		</div>
+		<?php } else { ?>
+			<span class="gray_shade1_text">Device will not boot</span>
+		<?php } ?>
 	</td>
 	<td>
-		<div class='frg-select-container color-light full_width'>
-			<select class="js-required" autocomplete="off">
-				<option selected>Select</option>
-				<option>Yes</option>
-				<option>No</option>
-			</select>
-		</div>
+		<?php if ( $page !== 'confirm_repair' ) { ?>
+			<div class='frg-select-container color-light full_width'>
+				<select class="js-required" autocomplete="off">
+					<option selected>Select</option>
+					<option>Yes</option>
+					<option>No</option>
+				</select>
+			</div>
+		<?php } else { ?>
+			<span class="gray_shade1_text">Yes</span>
+		<?php } ?>
 	</td>
 	<td class="right_align">
 		<span class="purple_text">$250.00</span>
