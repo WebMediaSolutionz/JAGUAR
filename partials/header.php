b9c41f1b9e099725c44e4cd9f80c2f30b1a8d5ed
Maxime Pierre <maxime.pierre@telus.com>
2016-04-18 14:20:15 -0400

    [EOFR-3922]: change menu link to 'BYOD plans'

--- partials/header.php
+++ partials/header.php
@@ -100,7 +100,7 @@
 							</li>
 							<li class="left">
 								<a class="<?php echo ( $page === 'plans' || $page === 'plans2' || $page === 'upgrades_plans' ) ? 'current ': ''; ?>block" href="plans.php">
-									<strong>Activate BYOD plan</strong>
+									<strong>BYOD plans</strong>
 								</a>
 							</li>
 							<!-- <li class="left"><a class="block" href="#">BYOD Setup</a></li> -->
