6f1757875b163f7cd832fa04df27d7469bb1ab2b
Maxime Pierre <maxime.pierre@telus.com>
2016-04-25 16:58:04 -0400

    [EOFR-3985]: final tweeks

--- order_approval.php
+++ order_approval.php
@@ -4,7 +4,9 @@
 <div class="outer-container secondary_bg white_bg">
 	<div class="inner-container top_bottom_padding clearfix">
 		<div class="left">
-			<p class="h1">Confirm order</p>
+			<p class="h1">Order summary: #123456789</p>
+
+			<h4 class="no_padding gray_text">Status: <span class="frg-icon icon-warning-inverted"></span> <span>Pending approval</span></h4>
 		</div>
 	</div>
 </div>
@@ -31,15 +33,7 @@
 	<div class="outer-container secondary_bg white_bg top_margin20 bottom_margin20">
 		<div class="inner-container clearfix">
 			<div class="left gray_text">
-				<span>Select hardware payment method:</span>
-
-				<div class='frg-select-container color-light'>
-					<select>
-						<option>Select</option>
-						<option>BAN: 555555555</option>
-						<option>BAN: 444444444</option>
-					</select>
-				</div>
+				<span>Select hardware payment method: BAN 55555555</span>
 			</div>
 		</div>
 	</div>
@@ -49,7 +43,7 @@
 	<div class="inner-container clearfix">
 		<table class="wide">
 			<thead class="gray_text">
-				<tr class="outline">
+				<tr>
 					<th>Item</th>
 					<th>Quantity</th>
 					<th>Description</th>
@@ -59,7 +53,7 @@
 				</tr>
 			</thead>
 			<tbody class="small gray_text">
-				<tr class="wide outline">
+				<tr class="wide">
 					<td class="item">
 						<span>Order-1</span>
 					</td>
@@ -86,7 +80,8 @@
 						<span>$25,500.00</span>
 					</td>
 				</tr>
-				<tr class="wide outline">
+				<?php include( 'partials/order_details_subscriber_row.php' ); ?>
+				<tr class="wide">
 					<td class="item">
 						<span>Order-2</span>
 					</td>
@@ -113,7 +108,8 @@
 						<span>$25,500.00</span>
 					</td>
 				</tr>
-				<tr class="wide outline">
+				<?php include( 'partials/order_details_subscriber_row.php' ); ?>
+				<tr class="wide">
 					<td class="item">
 						<span>Order-3</span>
 					</td>
@@ -140,96 +136,7 @@
 						<span>$25,500.00</span>
 					</td>
 				</tr>
-				<tr class="wide hide">
-					<td colspan="10">
-						<div class="subscriber_list purple_bg hide">
-							<table class="wide center">
-								<thead>
-									<tr>
-										<th>Subscriber</th>
-										<th>Porting</th>
-										<th>Details</th>
-										<th>SIM details</th>
-										<th>IMEI</th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr class="wide">
-										<td class="item">
-											<span>John Doe</span>
-										</td>
-										<td>
-											<span>No</span>
-										</td>
-										<td class="description">
-											<span>City: Ottawa | Preferred area code: 613 | Voicemail in English</span>
-										</td>
-										<td>
-											<span>Nano SIM ($10)</span>
-										</td>
-										<td>
-											<span>41255356535444</span>
-										</td>
-									</tr>
-									<tr class="wide">
-										<td class="item">
-											<span>John Doe</span>
-										</td>
-										<td>
-											<span>No</span>
-										</td>
-										<td class="description">
-											<span>City: Ottawa | Preferred area code: 613 | Voicemail in English</span>
-										</td>
-										<td>
-											<span>Nano SIM ($10)</span>
-										</td>
-										<td>
-											<span>41255356535444</span>
-										</td>
-									</tr>
-									<tr class="wide">
-										<td class="item">
-											<span>John Doe</span>
-										</td>
-										<td>
-											<span>No</span>
-										</td>
-										<td class="description">
-											<span>City: Ottawa | Preferred area code: 613 | Voicemail in English</span>
-										</td>
-										<td>
-											<span>Nano SIM ($10)</span>
-										</td>
-										<td>
-											<span>41255356535444</span>
-										</td>
-									</tr>
-									<tr class="wide">
-										<td class="item">
-											<span>John Doe</span>
-										</td>
-										<td>
-											<span>Yes</span>
-										</td>
-										<td class="description">
-											<span>Account: 2222222 | Phone Number: (555) 555 5555 | Voicemail in English</span>
-										</td>
-										<td>
-											<span>Nano SIM ($10)</span>
-										</td>
-										<td>
-											<span>41255356535444</span>
-										</td>
-									</tr>
-								</tbody>
-							</table>
-							<div class="heading clearfix">
-								<div class="left gap_left"><a class="js-hide_subscribers_list" href="#"><span>Hide list</span></a></div>
-							</div>
-						</div>
-					</td>
-				</tr>
+				<?php include( 'partials/order_details_subscriber_row.php' ); ?>
 			</tbody>
 		</table>
 	</div>
@@ -248,15 +155,7 @@
 	<div class="outer-container secondary_bg white_bg top_margin20 bottom_margin20">
 		<div class="inner-container clearfix">
 			<div class="left gray_text">
-				<span>Select hardware payment method:</span>
-
-				<div class='frg-select-container color-light'>
-					<select>
-						<option>Select</option>
-						<option>BAN: 555555555</option>
-						<option>BAN: 444444444</option>
-					</select>
-				</div>
+				<span>Select hardware payment method: BAN 66666666</span>
 			</div>
 		</div>
 	</div>
@@ -266,17 +165,17 @@
 	<div class="inner-container clearfix">
 		<table class="wide">
 			<thead class="gray_text">
-				<tr class="outline">
+				<tr>
 					<th>Item</th>
 					<th>Quantity</th>
 					<th>Description</th>
-					<th>Estimated arrival</th>
+					<th>Availability</th>
 					<th class="right_align">Due monthly</th>
 					<th class="right_align">Due now</th>
 				</tr>
 			</thead>
 			<tbody class="small gray_text">
-				<tr class="wide outline">
+				<tr class="wide">
 					<td class="item">
 						<span>Order-1</span>
 					</td>
@@ -303,7 +202,8 @@
 						<span>$25,500.00</span>
 					</td>
 				</tr>
-				<tr class="wide outline">
+				<?php include( 'partials/order_details_subscriber_row.php' ); ?>
+				<tr class="wide">
 					<td class="item">
 						<span>Order-2</span>
 					</td>
@@ -311,21 +211,12 @@
 						<span>50</span>
 					</td>
 					<td class="description">
-						<p class="big no_margin">Samsung Galaxy Grand Prime</p>
-						<ul class="gap_left_small">
-							<li>No commitment, contracts or credit checks</li>
-							<li>Low calling rates of $0.15 per min</li>
-							<li>Voice Mail 3, call display, call waiting and conference calling for free</li>
-						</ul>
-
-						<a class="js-show_list" href="#">View list</a>
+						<span>iPhone Case</span>
 					</td>
 					<td>
 						<span class="status positive">5-10 days</span>
 					</td>
-					<td class="right_align amount_due">
-						<span>$5,500.00</span>
-					</td>
+					<td class="right_align amount_due"></td>
 					<td class="right_align amount_due">
 						<span>$25,500.00</span>
 					</td>
@@ -337,37 +228,36 @@
 
 <div class="outer-container content white_bg order_summary small">
 	<div class="inner-container clearfix">
-		<div class="col-xs-6 col-xs-offset-6 no_padding clearfix">
-			<div class="right">
-				<table class="right_align subtotal">
-					<tr>
-						<td>Subtotal:</td>
-						<td class="right_align">$25,500.00</td>
-					</tr>
-					<tr>
-						<td>Shipping:</td>
-						<td class="right_align">Free</td>
-					</tr>
-					<tr>
-						<td>Taxes HST:</td>
-						<td class="right_align">$25,500.00</td>
-					</tr>
-				</table>
-			</div>
+		<div class="col-xs-6">
+			<p class="h3 gray_text">Approvers comments:</p>
+			<input class='frg-input-field textarea big full_width' />
 		</div>
-	</div>
-</div>
-
-<div class="outer-container content white_bg order_summary small">
-	<div class="inner-container clearfix">
-		<div class="col-xs-6 col-xs-offset-6 top_horizontal_separator light_gray no_padding clearfix">
+		<div class="col-xs-6 no_padding clearfix">
 			<div class="right">
-				<table class="right_align total">
-					<tr>
-						<td>Total:</td>
-						<td class="right_align">$31,922.50</td>
-					</tr>
-				</table>
+				<div class="top_horizontal_separator light_gray">
+					<table class="right_align subtotal">
+						<tr>
+							<td>Sub Total</td>
+							<td class="right_align">$25,500.00</td>
+						</tr>
+						<tr>
+							<td>Shipping</td>
+							<td class="right_align">Free</td>
+						</tr>
+						<tr>
+							<td>Taxes HST</td>
+							<td class="right_align">$25,500.00</td>
+						</tr>
+					</table>
+				</div>
+				<div class="top_horizontal_separator bottom_horizontal_separator light_gray">
+					<table class="right_align total">
+						<tr>
+							<td>Total</td>
+							<td class="right_align">$31,922.50</td>
+						</tr>
+					</table>
+				</div>
 			</div>
 		</div>
 	</div>
