e0fe9784a00dff759aebd1dc554ad7de81c4bce4
Maxime Pierre <maxime.pierre@telus.com>
2016-04-26 16:47:16 -0400

    [EOFR-3971]: Info to be displayed in Client OM

---
+++ partials/order_management_row.php
@@ -0,0 +1,32 @@
+<tr>
+	<td>
+		<div class="frg-checkbox small regular">
+			<div class="inner">
+				<input type="radio" value="Option 1" name="option" id="radio-option1">
+				<div class="icon"></div>
+				<label for="radio-option1"></label>
+			</div>
+		</div>
+	</td>
+	<td>
+		<a class="underline" href="order_details1.php?status=<?php echo ( $i % 2 === 0 ) ? 'shipped' : 'pending' ; ?>">1234567890</a>
+	</td>
+	<td>
+		<span>Stan a</span>
+	</td>
+	<td>
+		<span>Airtime Account -70728106 Airtime Account -70728106 Airtime Account -70728106</span>
+	</td>
+	<td>
+		<span>Jan 15, 2016</span>
+	</td>
+	<td>
+		<span>Purchase</span>
+	</td>
+	<td>
+		<span class="status positive">Pending approval</span>
+	</td>
+	<td>
+		<span class="purple_text">$25,750</span>
+	</td>
+</tr>
