79d633b8616caee8651374414df364268926824b
Maxime Pierre <maxime.pierre@telus.com>
2016-06-11 01:17:06 -0400

    [EOFR-3799]: latest updates for return page

--- confirm_return.php
+++ confirm_return.php
@@ -1,12 +1,16 @@
 <?php include_once( 'config.php' ); ?>
 <?php include_once( 'partials/header.php' ); ?>
 
-<?php include( 'partials/check_progress_bar.php' ); ?>
+<?php include( 'partials/check_progress_bar3.php' ); ?>
 
 <div class="outer-container secondary_bg white_bg">
 	<div class="inner-container clearfix">
 		<div class="left">
-			<p class="h1 no_padding purple_text">Confirm return</p>
+			<h3>Confirm return</h3>
+
+			<div class="top_bottom_small_padding gray_text">
+				<strong>Shipping address</strong><br /><span>Jonathan Richman, 6b-123 Main St., Ottawa, Ontario, A1B C2D, 613-555-1234</span>
+			</div>
 		</div>
 	</div>
 </div>
@@ -16,22 +20,14 @@
 		<table class="gray_text wide datagrid">
 			<thead class="gray_text">
 				<tr>
-					<?php if ( $page !== 'confirm_return' ) { ?>
-						<th>Select</th>
-					<?php } ?>
-					<th>Original order</th>
-					<th>Device Type</th>
-					<th>
-						<div class="left">IMEI</div> <div class="frg-icon icon-info back-order purple top_margin_minus7 left gap_left_small"></div>
-					</th>
+					<th>Device Information</th>
 					<th>Reason for return</th>
-					<th>Additional notes</th>
-					<th>Return Due</th>
+					<th class="right_align">Return Due</th>
 				</tr>
 			</thead>
 			<tbody>
 				<?php
-					for ( $i = 0; $i < 3; $i++ ) { 
+					for ( $i = 0; $i < 4; $i++ ) { 
 						include( 'partials/return_item_row.php' );
 					}
 				?>
@@ -46,16 +42,16 @@
 			<div class="right">
 				<table class="right_align subtotal">
 					<tr>
-						<td class="longer">Sub total</td>
-						<td class="right_align">$4,000.00</td>
+						<td class="longer gray_text top_bottom_small_padding">Sub total</td>
+						<td class="right_align top_bottom_small_padding">$4,000.00</td>
 					</tr>
 					<tr>
-						<td>Shipping</td>
-						<td class="right_align">Free</td>
+						<td class="gray_text top_bottom_small_padding">Shipping</td>
+						<td class="right_align top_bottom_small_padding">Free</td>
 					</tr>
 					<tr>
-						<td>Taxes HST</td>
-						<td class="right_align">$520.00</td>
+						<td class="gray_text top_bottom_small_padding">Taxes HST</td>
+						<td class="right_align top_bottom_small_padding">$520.00</td>
 					</tr>
 				</table>
 			</div>
@@ -69,8 +65,8 @@
 			<div class="right">
 				<table class="right_align total">
 					<tr>
-						<td class="longer">Total return</td>
-						<td class="right_align">$4,520.00</td>
+						<td class="longer gray_text top_bottom_small_padding">Total return</td>
+						<td class="right_align top_bottom_small_padding">$4,520.00</td>
 					</tr>
 				</table>
 			</div>
